bb5f3c28a9efa06696583ed3c3296c30d0ee45f4
yunhwan <79188190+yhyoon1004@users.noreply.github.com>
2022-09-01 15:05:53 +0900

    Model 처리, ModelView로 viewResolver 처리

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 	private String viewPath;
@@ -18,4 +19,13 @@ public class MyView {
 		dispatcher.forward(request, response);
 	}
 
+	public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		modelToRequestAttribute(model, request);
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		dispatcher.forward(request, response);
+	}
+
+	private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+		model.forEach((key, value)-> request.setAttribute(key,value));
+	}
 }
